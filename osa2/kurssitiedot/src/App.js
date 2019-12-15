400d8234b537e5f65c4802971be9f39dff1bbf5c
tkiviharju <tomas.kiviharju@gmail.com>
2019-12-15 23:07:30 +0200

    2.2. & 2.3.

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -4,31 +4,41 @@ import Course from './Course.jsx';
 
 const App = () => {
 	const course = {
-	  name: 'Half Stack application development',
-	  parts: [
-		{
-		  name: 'Fundamentals of React',
-		  exercises: 10,
-		  id: 1
-		},
-		{
-		  name: 'Using props to pass data',
-		  exercises: 7,
-		  id: 2
-		},
-		{
-		  name: 'State of a component',
-		  exercises: 14,
-		  id: 3
-		}
-	  ]
-	}
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10,
+				id: 1
+			},
+			{
+				name: 'Using props to pass data',
+				exercises: 7,
+				id: 2
+			},
+			{
+				name: 'State of a component',
+				exercises: 14,
+				id: 3
+			},
+			{
+				name: 'Redux',
+				exercises: 11,
+				id: 4
+			}
+		]
+	};
+
+	const total = course.parts.reduce((prevValue, nextPart) => prevValue + nextPart.exercises, 0);
   
 	return (
-	  <div>
-		<Course course={course} />
-	  </div>
-	)
-  }
+		<div>
+			<Course course={course} />
+			<strong>
+				total of {total} exercises
+			</strong>
+	  	</div>
+	);
+}
 
 export default App;
